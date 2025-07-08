cb6c01332584d65700d4f3d24b01ce51d287c549
BlooshedDev <anggerbagas.s@gmail.com>
2025-07-08 12:17:18 +0700

    Archive

---
+++ archive/main.js
@@ -0,0 +1,20 @@
+function itchio() {
+  window.location.href = "https://blooshed-developer.itch.io/";
+}
+function github() {
+  window.location.href = "https://github.com/BlooshedDev";
+}
+
+// let proccessScroll = () => {
+//   let docElement = document.documentElement;
+//   let docBody = document.body;
+//   let scrollTop = docElement.scrollTop || docBody.scrollTop;
+//   let scrollBottom =
+//     (docElement.scrollHeight || docBody.scrollHeight) - window.innerHeight;
+//   let scrollPercent = (scrollTop / scrollBottom) * 360;
+//   console.log(scrollPercent);
+
+//   const logo = document.querySelector(".logo");
+//   logo.style.transform = `rotate(${scrollPercent}deg)`;
+// };
+// document.addEventListener("scroll", proccessScroll);
